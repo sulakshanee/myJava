8421217adb4552e784cc891797d6bad49b9c8da8
sulakshanee <sulakshanee111@gmail.com>
2021-04-27 03:24:43 +0530

    math_Java

---
+++ BasicJava/MathClass.java
@@ -0,0 +1,32 @@
+public class MathClass {
+    public static void main(String[] args) {
+
+        // Max
+        System.out.println(Math.max(77, 19));
+
+        // Min
+        System.out.println(Math.min(55, 67));
+
+        // Squrt
+        System.out.println(Math.sqrt(64));
+
+        // Absolute(Positive) Value
+        System.out.println(Math.abs(-110.20));
+
+        // Round
+        System.out.println(Math.round(230.235));
+
+        // Rounded Up
+        System.out.println(Math.ceil(56.78));
+
+        // Rounded Down
+        System.out.println(Math.floor(56.78));
+
+        // Power
+        System.out.println(Math.pow(2, 8));
+
+        // Random
+        System.out.println(Math.random());
+        System.out.println((int) (Math.random() * 101)); // 0 to 100
+    }
+}
