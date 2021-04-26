fba8148d944c836ea60e6091ee782028d25ed64e
sulakshanee <sulakshanee111@gmail.com>
2021-04-27 02:48:42 +0530

    new java

--- BasicJava/TypeCasting.java
+++ BasicJava/TypeCasting.java
@@ -6,17 +6,17 @@ public class TypeCasting {
         int b = a;
         double c = b;
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.println(a); // 20
+        System.out.println(b); // 20
+        System.out.println(c); // 20.0
 
         // Narrowing Casting -: byte <- short <- char <- int <- long <- float <- double
         double x = 120.51;
         int y = (int) x;
         short z = (short) y;
 
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(z);
+        System.out.println(x); // 120.51
+        System.out.println(y); // 120
+        System.out.println(z); // 120
     }
 }
